e142a542ca4bbcb5ec7fee20e02f96606624cb41
Rohit Nautiyal <85772545+Nauti-Rohit@users.noreply.github.com>
2024-04-05 17:45:34 +0530

    Update App.jsx
    again try to fix bgImg

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import "./App.css";
 import WeatherApp from "./WeatherApp";
 
 function App() {
+  let bgImg= "https://images.unsplash.com/photo-1580193769210-b8d1c049a7d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
   return (
     <>
-      <WeatherApp style={{ backgroundImage: `url(${https://www.saga.co.uk/contentlibrary/saga/publishing/verticals/technology/apps/shutterstock_240459751.jpg})` }} />
+      <WeatherApp style={{ backgroundImage: `url(${bgImg})` }} />
     </>
   );
 }
